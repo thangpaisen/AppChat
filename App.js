747617e723682c731da07b4b4add9ee829061b4e
thangquan <doraemona1k44@gmail.com>
2021-07-19 10:41:34 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World2!</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
